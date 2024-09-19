d058e06dbc238861e3e84533cfb76ea7edc2cbd7
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-09-19 21:33:10 +0700

    test: properties

---
+++ 03-typescript-oop/test/properties.test.ts
@@ -0,0 +1,33 @@
+describe("Properties", () => {
+  class Customer {
+    readonly id: number;
+    name: string;
+    // kode: properties default value
+    // name: string = "Guest"
+    // kode: properties default value
+    age?: number;
+
+    constructor(id: number, name: string) {
+      this.id = id;
+      this.name = name;
+    }
+    // kode: properties default value
+    // constructor(id: number) {
+    //   this.id = id;
+    // }
+    // kode: properties default value
+  }
+
+  it("should can have properties", () => {
+    const customer = new Customer(1, "Gilbert");
+    // kode: properties default value
+    // const customer = new Customer(1);
+    // kode: properties default value
+    customer.age = 20;
+
+    console.info(customer.id);
+    console.info(customer.name);
+    console.info(customer.age);
+    console.info(customer);
+  });
+});
