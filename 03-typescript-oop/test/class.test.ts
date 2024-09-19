80622198d74ae8214973a13be51b4169be4d08d9
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-09-19 15:03:31 +0700

    test: class

---
+++ 03-typescript-oop/test/class.test.ts
@@ -0,0 +1,10 @@
+describe("Class", () => {
+  it("should can create class", () => {
+    class Customer {}
+
+    class Order {}
+
+    const customer: Customer = new Customer();
+    const order = new Order();
+  });
+});
