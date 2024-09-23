d71a8db8e49f5808343bd94edc55ffa9ea76e097
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-09-23 10:42:56 +0700

    test: inheritance interface

---
+++ 03-typescript-oop/test/interface.test.ts
@@ -0,0 +1,26 @@
+describe("Interface", () => {
+  interface HasName {
+    name: string;
+  }
+
+  interface CanSayHello {
+    sayHello(name: string): void;
+  }
+
+  class Person implements HasName, CanSayHello {
+    name: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    sayHello(name: string): void {
+      console.info(`Hello ${name}, my name is ${this.name}`);
+    }
+  }
+
+  it("should sipport inheritance", () => {
+    const person = new Person("Gilbert");
+    person.sayHello("Budi");
+  });
+});
