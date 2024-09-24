2bad10f3bf726352baaf2974b5293deb608be72e
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-09-24 18:32:00 +0700

    test: method overriding

---
+++ 03-typescript-oop/test/method-overriding.test.ts
@@ -0,0 +1,28 @@
+describe("Method Overrding", () => {
+  class Employee {
+    name: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    sayHello(name: string): void {
+      console.info(`Hello ${name}, my name is ${this.name}`);
+    }
+  }
+
+  class Manager extends Employee {
+    sayHello(name: string): void {
+      console.info(
+        `Hello ${name}, my name is ${this.name}, I am  your manager`
+      );
+    }
+  }
+
+  it("should support method overding", () => {
+    const employee = new Employee("Gilbert");
+    employee.sayHello("Budi");
+    const manager = new Manager("Gilbert");
+    manager.sayHello("Budi");
+  });
+});
