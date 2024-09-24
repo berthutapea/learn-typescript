70e09e13c68d04fe0ad6661a8e8f63770c3262ad
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-09-24 20:32:22 +0700

    test: visibility

---
+++ 03-typescript-oop/test/visibility.test.ts
@@ -0,0 +1,35 @@
+describe("Visibility", () => {
+  class Counter {
+    protected counter: number = 0;
+
+    public increment(): void {
+      this.counter++;
+    }
+
+    public getCounter(): number {
+      return this.counter;
+    }
+  }
+
+  class DoubleCounter extends Counter {
+    public increment(): void {
+      this.counter += 2;
+    }
+  }
+
+  it("should support private", () => {
+    const counter = new Counter();
+    counter.increment();
+    counter.increment();
+    counter.increment();
+    console.info(counter.getCounter());
+  });
+
+  it("should support proctected", () => {
+    const counter = new DoubleCounter();
+    counter.increment();
+    counter.increment();
+    counter.increment();
+    console.info(counter.getCounter());
+  });
+});
