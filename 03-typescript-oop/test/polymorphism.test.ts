dca01fe9225d48c45acdc875d8c0c7c7bebe1bae
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-01 02:34:16 +0700

    test: type cast

--- 03-typescript-oop/test/polymorphism.test.ts
+++ 03-typescript-oop/test/polymorphism.test.ts
@@ -7,8 +7,34 @@ describe("Polymorphism", () => {
 
   class VicePresident extends Manager {}
 
+  //   function sayHello(employee: Employee): void {
+  //     console.info(`Hello ${employee.name}`);
+  //   }
+
+  //   code type cast true
   function sayHello(employee: Employee): void {
-    console.info(`Hello ${employee.name}`);
+    if (employee instanceof VicePresident) {
+      const vp = employee as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else if (employee instanceof Manager) {
+      const manager = employee as Manager;
+      console.info(`Hello manager ${manager.name}`);
+    } else {
+      console.info(`Hello employee ${employee.name}`);
+    }
+  }
+
+  //   code type cast false
+  function sayHelloWrong(employee: Employee): void {
+    if (employee instanceof Manager) {
+      const manager = employee as Manager;
+      console.info(`Hello manager ${manager.name}`);
+    } else if (employee instanceof VicePresident) {
+      const vp = employee as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else {
+      console.info(`Hello employee ${employee.name}`);
+    }
   }
 
   it("should support", () => {
@@ -22,9 +48,15 @@ describe("Polymorphism", () => {
     console.info(employee);
   });
 
-  it("should support method parameter polymorphism ", () => {
+  it("should support method parameter polymorphism true", () => {
     sayHello(new Employee("Gilbert"));
     sayHello(new Manager("Aldi"));
     sayHello(new VicePresident("Yusdin"));
+  });
+
+  it("should support method parameter polymorphism false", () => {
+    sayHelloWrong(new Employee("Gilbert"));
+    sayHelloWrong(new Manager("Aldi"));
+    sayHelloWrong(new VicePresident("Yusdin"));
   });
 });
