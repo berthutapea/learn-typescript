446acc206dfce9d91d341196c317a93e7667761b
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-07 11:46:36 +0700

    test: multiple generic type

--- 04-typescript-generic/test/generic.test.ts
+++ 04-typescript-generic/test/generic.test.ts
@@ -39,4 +39,22 @@ describe("Generic", () => {
     const result2: number = create<number>(123);
     expect(result2).toBe(123);
   });
+
+  class Entry<K, V> {
+    constructor(public key: K, public value: V) {}
+  }
+  class Triple<K, V, T> {
+    constructor(public first: K, public second: V, public third: T) {}
+  }
+
+  it("should suppor multiple generic type", async () => {
+    const entry = new Entry<number, string>(1, "Hello");
+    expect(entry.key).toBe(1);
+    expect(entry.value).toBe("Hello");
+
+    const triple = new Triple<number, string, boolean>(1, "Hello", true);
+    expect(triple.first).toBe(1);
+    expect(triple.second).toBe("Hello");
+    expect(triple.third).toBe(true);
+  });
 });
