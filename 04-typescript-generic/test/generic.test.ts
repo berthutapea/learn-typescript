8f982061028dc63d82151eaca3a7238469a5a56d
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-07 13:31:27 +0700

    test: optional generic type

--- 04-typescript-generic/test/generic.test.ts
+++ 04-typescript-generic/test/generic.test.ts
@@ -57,4 +57,31 @@ describe("Generic", () => {
     expect(triple.second).toBe("Hello");
     expect(triple.third).toBe(true);
   });
+
+  it("should support optional generic type", async () => {
+    const entry = new Entry(1, "Hello");
+    expect(entry.key).toBe(1);
+    expect(entry.value).toBe("Hello");
+  });
+
+  class SimpleGeneric<T> {
+    private value?: T;
+
+    setValue(value: T) {
+      this.value = value;
+    }
+
+    getValue(): T | undefined {
+      return this.value;
+    }
+  }
+
+  it("should create simple generic", async () => {
+    const simple = new SimpleGeneric<string>();
+    simple.setValue("Gilbert");
+    // simple.setValue(100)
+    // simple.setValue(true)
+
+    expect(simple.getValue()!.toUpperCase()).toBe("GILBERT");
+  });
 });
