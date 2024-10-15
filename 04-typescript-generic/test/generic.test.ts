72dac362f36a1a6c57d5ee50bf26ba3c4daae764
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-15 15:46:20 +0700

    test: generic promise

--- 04-typescript-generic/test/generic.test.ts
+++ 04-typescript-generic/test/generic.test.ts
@@ -151,4 +151,27 @@ describe("Generic", () => {
     expect(map.get("Gilbert")).toBe(100);
     expect(map.get("Hutapea")).toBe(90);
   });
+
+  async function fetchData(value: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      setTimeout(() => {
+        if (value === "Gilbert") {
+          resolve("Hello " + value);
+        } else {
+          reject("Not Found");
+        }
+      }, 1000);
+    });
+  }
+
+  it("should support promise", async () => {
+    const result = await fetchData("Gilbert");
+    expect(result.toUpperCase()).toBe("HELLO GILBERT");
+
+    try {
+      await fetchData("Budi");
+    } catch (e) {
+      expect(e).toBe("Not Found");
+    }
+  });
 });
