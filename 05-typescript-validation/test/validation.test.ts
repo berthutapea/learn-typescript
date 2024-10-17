13db40681e082968b8713aad9335ac9c29f050bd
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-17 11:46:39 +0700

    test: validation

---
+++ 05-typescript-validation/test/validation.test.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+describe("zod", () => {
+  it("should support validation", async () => {
+    const schema = z.string().min(3).max(100);
+
+    const request = "Gilbert";
+    const result = schema.parse(request);
+
+    expect(result).toBe("Gilbert");
+  });
+
+  it("should support validate primitive data type", async () => {
+    const usernameSchema = z.string().email();
+    const isAdminSchema = z.boolean();
+    const priceSchema = z.number().min(100).max(1000000);
+
+    const username = usernameSchema.parse("gilbert@example.com");
+    console.info(username);
+
+    const isAdmin = isAdminSchema.parse(true);
+    console.info(isAdmin);
+
+    const price = priceSchema.parse(100);
+    console.info(price);
+  });
+});
