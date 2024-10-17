da59c0d09b0afa80c87ad5126f49f3543059b352
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-17 16:43:30 +0700

    test: data conversion

--- 05-typescript-validation/test/validation.test.ts
+++ 05-typescript-validation/test/validation.test.ts
@@ -24,4 +24,19 @@ describe("zod", () => {
     const price = priceSchema.parse(100);
     console.info(price);
   });
+
+  it("should support data conversion", async () => {
+    const usernameSchema = z.coerce.string().min(3).max(100);
+    const isAdminSchema = z.coerce.boolean();
+    const priceSchema = z.coerce.number().min(100).max(1000000);
+
+    const username = usernameSchema.parse(1234);
+    console.info(username);
+
+    const isAdmin = isAdminSchema.parse("true");
+    console.info(isAdmin);
+
+    const price = priceSchema.parse("100");
+    console.info(price);
+  });
 });
