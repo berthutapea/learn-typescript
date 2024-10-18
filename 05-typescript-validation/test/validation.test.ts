ae3dcd4d8ddac10933dd2fcd22b573786b2afc30
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-19 00:12:30 +0700

    test: collection validation

--- 05-typescript-validation/test/validation.test.ts
+++ 05-typescript-validation/test/validation.test.ts
@@ -123,4 +123,38 @@ describe("zod", () => {
     const result = createUserSchema.parse(request);
     console.info(result);
   });
+
+  it("should support array validation", async () => {
+    const schema = z.array(z.string().email()).min(1).max(10);
+
+    const request: Array<string> = ["gilbert@example.com", "budi@example.com"];
+    const result: Array<string> = schema.parse(request);
+
+    console.info(result);
+  });
+
+  it("should support set validation", async () => {
+    const schema = z.set(z.string().email()).min(1).max(10);
+
+    const request: Set<string> = new Set([
+      "gilbert@example.com",
+      "budi@example.com",
+      "gilbert@example.com",
+    ]);
+    const result: Set<string> = schema.parse(request);
+
+    console.info(result);
+  });
+
+  it("should support map validation", async () => {
+    const schema = z.map(z.string(), z.string().email());
+
+    const request: Map<string, string> = new Map([
+      ["gilbert", "gilbert@example.com"],
+      ["budi", "budi@example.com"],
+    ]);
+
+    const result: Map<string, string> = schema.parse(request);
+    console.info(result);
+  });
 });
