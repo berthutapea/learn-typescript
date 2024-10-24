13e8fbcbda4be62763451991c287cfb0c80bc7da
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-25 02:39:21 +0700

    feat: get user api

--- 06-typescript-restful-api/src/service/user-service.ts
+++ 06-typescript-restful-api/src/service/user-service.ts
@@ -1,3 +1,4 @@
+import { User } from "@prisma/client";
 import { prismaClient } from "../application/database";
 import { ResponseError } from "../error/response-error";
 import {
@@ -70,5 +71,9 @@ export class UserService {
     const response = toUserResponse(user);
     response.token = user.token!;
     return response;
+  }
+
+  static async get(user: User): Promise<UserResponse> {
+    return toUserResponse(user);
   }
 }
--- 06-typescript-restful-api/test/user.test.ts
+++ 06-typescript-restful-api/test/user.test.ts
@@ -78,3 +78,34 @@ describe("POST /api/users/login", () => {
     expect(response.body.errors).toBeDefined();
   });
 });
+
+describe("GET /api/users/current", () => {
+  beforeEach(async () => {
+    await UserTest.create();
+  });
+
+  afterEach(async () => {
+    await UserTest.delete();
+  });
+
+  it("should be able to get user", async () => {
+    const response = await supertest(web)
+      .get("/api/users/current")
+      .set("X-API-TOKEN", "test");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe("test");
+    expect(response.body.data.name).toBe("test");
+  });
+
+  it("should reject get user if token is invalid", async () => {
+    const response = await supertest(web)
+      .get("/api/users/current")
+      .set("X-API-TOKEN", "salah");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(401);
+    expect(response.body.errors).toBeDefined();
+  });
+});
