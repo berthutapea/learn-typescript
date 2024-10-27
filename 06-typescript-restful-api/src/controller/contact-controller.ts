7d0ba4b11acf4a59a10394400f565e95d59b0e00
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-28 00:36:09 +0700

    feat: search contact api

--- 06-typescript-restful-api/src/controller/contact-controller.ts
+++ 06-typescript-restful-api/src/controller/contact-controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from "express";
 import { UserRequest } from "../type/user-request";
 import {
   CreateContactRequest,
+  SearchContactRequest,
   UpdateContactRequest,
 } from "../model/contact-model";
 import { ContactService } from "../service/contact-service";
@@ -52,6 +53,22 @@ export class ContactController {
       res.status(200).json({
         data: "OK",
       });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async search(req: UserRequest, res: Response, next: NextFunction) {
+    try {
+      const request: SearchContactRequest = {
+        name: req.query.name as string,
+        email: req.query.email as string,
+        phone: req.query.phone as string,
+        page: req.query.page ? Number(req.query.page) : 1,
+        size: req.query.size ? Number(req.query.size) : 10,
+      };
+      const response = await ContactService.search(req.user!, request);
+      res.status(200).json(response);
     } catch (e) {
       next(e);
     }
