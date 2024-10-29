f007eb54cd48090b571e2751d24b8d26ba474735
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-29 15:55:25 +0700

    feat: create address api

--- 06-typescript-restful-api/test/test-util.ts
+++ 06-typescript-restful-api/test/test-util.ts
@@ -72,3 +72,15 @@ export class ContactTest {
     return contact;
   }
 }
+
+export class AddressTest {
+  static async deleteAll() {
+    await prismaClient.address.deleteMany({
+      where: {
+        contact: {
+          username: "test",
+        },
+      },
+    });
+  }
+}
