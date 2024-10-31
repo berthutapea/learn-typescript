be8104027a6440034b3fb9cd6209ab12767f5eab
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-31 22:43:32 +0700

    fix: list address api

--- 06-typescript-restful-api/src/route/api.ts
+++ 06-typescript-restful-api/src/route/api.ts
@@ -38,3 +38,8 @@ apiRouter.delete(
   "/api/contacts/:contactId(\\d+)/addresses/:addressId(\\d+)",
   AddressController.remove
 );
+
+apiRouter.get(
+  "/api/contacts/:contactId(\\d+)/addresses",
+  AddressController.list
+);
